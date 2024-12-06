ae3898c63ad965423c1a70e9d4fc49a0bdfbdcfa
jyotishankar04 <patrajyotishankar@gmail.com>
2024-12-06 22:16:45 +0530

    adding react vite client and configure tailwindcss and nextui

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen ">
+      <Button variant="ghost">Button</Button>
+    </div>
+  );
+};
+
+export default App;
