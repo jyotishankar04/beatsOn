79dc3997eec3873140cc8b4c7b3ec5f5cfde5087
jyotishankar04 <patrajyotishankar@gmail.com>
2024-12-07 21:31:06 +0530

    adding react vite client and configure tailwindcss and nextui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@nextui-org/react";
-
 const App = () => {
   return (
-    <div className="w-full h-screen ">
-      <Button variant="ghost">Button</Button>
+    <div className="text-3xl bg-red-500 text-white p-5 font-bold">
+      Hello, Vite!
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,12 @@
-// tailwind.config.js
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+	"./index.html",
+	"./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()]
+  plugins: [],
 }
+
