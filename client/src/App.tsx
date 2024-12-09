1d1eb16ba5403b0ad7aae00b3b1733598f1b6477
jyotishankar04 <patrajyotishankar@gmail.com>
2024-12-09 22:12:43 +0530

    cleaning up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,5 @@
-import { Button } from "@nextui-org/button";
-import { useState } from "react";
 const App = () => {
-  const [name, setName] = useState("Tanmayee");
-  return (
-    <div className="w-full h-screen flex-col gap-10 flex justify-center items-center bg-black">
-      <h1 className="text-red-800 font-bold text-4xl bg-black">{name}</h1>
-      <Button
-        color="primary"
-        onClick={() => {
-          setName("Kundu");
-        }}
-      >
-        Change
-      </Button>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default App;
